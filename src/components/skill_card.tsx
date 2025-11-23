c30a427e45de6d868827645b4189b461908778e0
arca0928 <arca0928@gmail.com>
2025-11-23 23:24:36 +0900

    feat: add responsive

--- src/components/skill_card.tsx
+++ src/components/skill_card.tsx
@@ -8,16 +8,19 @@ type SkillCardProps = {
 
 const SkillCard = (props: SkillCardProps) => {
 	return (
-		<Card className="flex flex-row w-45 h-50 m-4" isHoverable>
+		<Card
+			className="flex flex-row w-41 h-48 m-5 md:m-4 md:w-33 md:h-40 grow"
+			isHoverable
+		>
 			<CardBody>
-				<img
-					src={props.icon}
-					alt={props.alt ?? props.title}
-					className="w-25 mx-auto"
-				/>
-				<p className="text-center mt-4 font-semibold text-lg">
-					{props.title}
-				</p>
+				<div className="w-25 h-25 mx-auto flex items-center justify-center">
+					<img
+						src={props.icon}
+						alt={props.alt ?? props.title}
+						className="max-w-full max-h-full object-contain"
+					/>
+				</div>
+				<p className="text-center mt-4 font-semibold text-lg">{props.title}</p>
 			</CardBody>
 		</Card>
 	);
